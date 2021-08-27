753b6ed40a929f0d2b7cdb1d63532fdb94603d42
Williams-bo <williamssunday170@gmail.com>
2021-08-27 02:03:03 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int result = 0;
+
+            // first number
+            Console.Write("Please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            // second number
+            Console.Write("Please enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            // Task 2 - Division
+            result = firstNumber - secondNumber;
+            Console.WriteLine("The answer is: " + result);
+
+        }
+    }
+}
